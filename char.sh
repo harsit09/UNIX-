1a4ae292574559fd7abdf92c45c29dbc147a4b22
harsit09 <84085808+harsit09@users.noreply.github.com>
2023-01-31 21:23:44 +0530

    Create char.sh

---
+++ char.sh
@@ -0,0 +1,10 @@
+Shell script to check number of lines, words, characters in a file
+#!/bin/bash
+echo "Enter the filename or path to proceed"
+read filename
+words=`wc -w $filename`
+lines=`wc -l $filename`
+chars=`wc -c $filename`
+echo "Words is $words"
+echo "Lines is $lines"
+echo "Characters is $chars"
